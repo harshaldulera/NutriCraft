967d857ac690cf8cd0127d8e6ad0a302ac5860a0
Apurva Dharam <aadharam2004@gmail.com>
2024-02-03 18:59:51 +0530

    login css

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -14,9 +14,9 @@ const Login = () => {
   }, [user, navigate]);
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-secondary-100 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <h1 className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+        <h1 className="flex items-center mb-6 text-2xl font-semibold text-secondary-50 dark:text-white">
           NutriCraft
         </h1>
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
