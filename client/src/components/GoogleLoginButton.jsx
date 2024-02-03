1333086b769660b0092fdd517eda649cf8b4e8cc
Harshal Dulera <harshaldulera82@gmail.com>
2024-02-03 13:57:06 +0530

    feat: google button

---
+++ client/src/components/GoogleLoginButton.jsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import GoogleButton from "react-google-button";
+
+const GoogleLoginButton = () => {
+    return <GoogleButton />;
+}
+
+export default GooleLoginButton;
