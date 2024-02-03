c3458df07a814f98c5c324e07aaa1135e77f392c
Rahil Kothari <kotharirahil9@gmail.com>
2024-02-03 22:13:43 +0530

    change styling
    Co-authored-by: Aayush Mehta <aayushmehta02@users.noreply.github.com>

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -5,7 +5,7 @@ import Upload from "./components/Upload";
 import Signup from "./pages/Auth/Signup";
 import Parameters  from "./components/Parameters";
 import CopyUpload from "./components/CopyUpload";
-import Heatmap from "./components/Heatmap";
+import Barchart from "./components/Barchart";
 export const Router = () => {
     return (
         <>
@@ -16,7 +16,7 @@ export const Router = () => {
                 <Route path='copyupload' element={<CopyUpload/>}/>
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/parameters" element={<Parameters ingredients="flour sugar butter eggs milk baking powder vanilla"/>}/>
-                <Route path="/dashboard" element={<Heatmap />} />
+                <Route path="/dashboard" element={<Barchart />} />  
             </Routes>
         </>
     );
